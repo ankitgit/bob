b07774e6d2c8179076170f530233d0dd99ea6f75
Ankit Chaudhary <my007id@gmail.com>
2019-04-14 21:12:56 +0200

    Updated the code

--- src/main/java/io/ankitgit/runner/scheduler/LoadConsumer.java
+++ src/main/java/io/ankitgit/runner/scheduler/LoadConsumer.java
@@ -1,17 +1,10 @@
 package io.ankitgit.runner.scheduler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.ankitgit.runner.controller.PassengerLoadController;
-import io.ankitgit.runner.model.Compartment;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Properties;
 
 
 @Component
@@ -22,27 +15,7 @@ public class LoadConsumer {
     @Scheduled(fixedRate = 5000)
     public void consumeMessage() throws IOException {
 
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("session.timeout.ms", "30000");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("kafka_output"));
-
-        PassengerLoadController passengerLoadController = new PassengerLoadController();
-
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records) {
-                Compartment compartment = objectMapper.readValue(record.value(), Compartment.class);
-                String key = compartment.getVehicleId() + "" + compartment.getCompartmentId();
-                passengerLoadController.addValue(key, compartment);
-            }
-        }
+        System.out.printf("Hey Ho Hey Ho");
     }
 
 
